cc117f729e538c3e9294b7d1e24847aa47239bfd
beabruns <beatrizrbruns@gmail.com>
2026-07-16 22:48:21 -0300

    feat: implementa query 01 e adiciona CSV

---
+++ sql/query_01.sql
@@ -0,0 +1,16 @@
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    d.department_name,
+    j.job_title,
+    e.salary
+FROM HR.EMPLOYEES e
+LEFT JOIN HR.DEPARTMENTS d
+       ON e.department_id = d.department_id
+LEFT JOIN HR.JOBS j
+       ON e.job_id = j.job_id
+ORDER BY
+    d.department_name,
+    j.job_title,
+    e.salary DESC;
